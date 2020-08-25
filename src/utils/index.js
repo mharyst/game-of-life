ea8a5935cb310e0ccc58d9fdeb123d49f7b405b2
mykhailo.haryst <mykhailo.haryst@pm.bet>
2020-08-26 02:59:33 +0300

    Add flow

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,10 @@
+// @flow
 import {ROWS, COLUMNS, NEIGHBORS} from '../config'
 
-export const createInitialGrid = () =>
+type Grid = Array<Array<boolean>>
+
+type CreateInitialGrid = () => Grid
+export const createInitialGrid: CreateInitialGrid = () =>
   Array.from({length: ROWS})
     .map(
       () => Array.from({length: COLUMNS})
@@ -9,14 +13,17 @@ export const createInitialGrid = () =>
         ),
     )
 
-const calculateLiveNeighbors = ({cellIndex, rowIndex, grid}) => NEIGHBORS.map(
+type CalculateLiveNeighbors = ({cellIndex: number, rowIndex: number, grid: Grid}) => number
+const calculateLiveNeighbors: CalculateLiveNeighbors = ({cellIndex, rowIndex, grid}) => NEIGHBORS.map(
   ([x, y]) => grid[rowIndex + x] && grid[rowIndex + x][cellIndex + y],
 ).filter(Boolean).length
 
-export const recalculateGrid = (grid) => grid.map((row, rowIndex) => row.map((cell, cellIndex) => {
-  const liveNeighbors = calculateLiveNeighbors({cellIndex, grid, rowIndex})
+type RecalculateGrid = (grid: Grid) => Grid
+export const recalculateGrid: RecalculateGrid = (grid) => grid.map((row, rowIndex) =>
+  row.map((cell, cellIndex) => {
+    const liveNeighbors = calculateLiveNeighbors({cellIndex, grid, rowIndex})
 
-  return cell
-    ? liveNeighbors <= 3 && liveNeighbors >= 2
-    : liveNeighbors === 3
-}))
+    return cell
+      ? liveNeighbors <= 3 && liveNeighbors >= 2
+      : liveNeighbors === 3
+  }))
