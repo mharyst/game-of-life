232e50cf4b0a3eb55aff3f3729d7b3c3f6ca38c7
mykhailo.haryst <mykhailo.haryst@pm.bet>
2020-08-25 23:11:02 +0300

    Initial commit

---
+++ src/components/Grid/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import css from './style.css'
+
+const Grid = () => (
+  <div className={css.container}>
+    App
+  </div>
+)
+
+export default Grid
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: [
+    'babel-polyfill',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+}
+
+module.exports = config;
